a622ae33e39e7e211bdd564fa30cbeaf4f8ace07
Awa Melvine <melvineawa@gmail.com>
2019-05-21 14:34:48 +0100

    Create a Navbar component
    - Expand and collapse nav menu when menu icon is clicked
    - When a link is clicked, that link should be highlighted

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+class NavLink {
+  constructor(link) {
+    this.link = link;
+    this.navMenu = document.querySelector(".nav-menu");
+    this.menuIcon = document.querySelector(".menu-icon");
+
+    this.link.addEventListener("click", e => {
+      e.preventDefault();
+      this.select();
+    });
+
+    this.menuIcon.addEventListener("click", e => {
+      e.preventDefault();
+      console.log("test");
+      this.toggleNavMenu();
+    });
+  }
+
+  toggleNavMenu() {
+    this.navMenu.classList.toggle("open");
+  }
+
+  select() {
+    const links = document.querySelectorAll(".nav-menu li");
+    links.forEach(link => link.classList.remove("active"));
+    this.link.classList.add("active");
+  }
+}
+const links = document.querySelectorAll(".nav-menu li");
+
+links.forEach(link => new NavLink(link));
