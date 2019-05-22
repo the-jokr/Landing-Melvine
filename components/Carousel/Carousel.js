e2cb49c24359fadb18e22616a8045c1215cf0863
Awa Melvine <melvineawa@gmail.com>
2019-05-22 14:42:48 +0100

    Create a javascript carousel component for the reviews

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,29 @@
+class Carousel {
+  constructor(element) {
+    this.element = element;
+    this.slides = this.element.querySelectorAll(".slide");
+    this.currentIndex = 0;
+
+    this.deselect();
+    this.setCurrentSlide();
+  }
+
+  deselect() {
+    this.slides.forEach(slide => {
+      slide.classList.remove("show");
+    });
+  }
+  setCurrentSlide() {
+    this.deselect();
+    let slides = document.querySelectorAll(".slide");
+
+    slides.forEach((slide, index) => {
+      if (index === this.currentIndex) {
+        slide.classList.add("show");
+      }
+    });
+  }
+}
+
+let carousel = document.querySelector(".carousel");
+new Carousel(carousel);
